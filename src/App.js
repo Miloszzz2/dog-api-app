3d6265ef5e54e2ec9999aba6b5cc524fdf69f160
Miloszzz2 <97192271+Miloszzz2@users.noreply.github.com>
2022-10-14 17:53:47 +0200

    final changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
+  const [dogs, setDogs] = useState([]);
+  const [image, setImage] = useState();
+  const inputSelect = useRef(null);
+
+  const loadDogs = async () => {
+    await fetch('https://dog.ceo/api/breeds/list/all')
+      .then((response) => response.json())
+      .then((data) => {
+        setDogs(data.message);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    loadDogs();
+  }, []);
+
+  const generateImage = async () => {
+    await fetch(
+      `https://dog.ceo/api/breed/${inputSelect.current.value}/images/random`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.message);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className='App'>
+        <select
+          onChange={(e) => {
+            generateImage();
+          }}
+          ref={inputSelect}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option>Select breed</option>
+          {dogs &&
+            Object.keys(dogs).map((item) => {
+              return (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              );
+            })}
+        </select>
+
+        {image && (
+          <>
+            <img src={image} alt='' />
+            <button onClick={() => generateImage()}>Generate another</button>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
